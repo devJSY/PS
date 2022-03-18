ea5cf320679ed1a0498918911f8da093f5686f83
Seungyeon Jung <wjdtmd486@naver.com>
2022-03-18 20:13:41 +0900

    add

--- 백준 온라인 저지/단계 별 문제풀기/8.기본 수학 1/1193.py
+++ 백준 온라인 저지/단계 별 문제풀기/8.기본 수학 1/1193.py
@@ -1,16 +1,16 @@
-# X=int(input())
+X=int(input())
 
-# line=1
-# while X>line:
-#     X-=line
-#     line+=1
+line=1
+while X>line:
+    X-=line
+    line+=1
     
-# if line%2==0:
-#     a=X
-#     b=line-X+1
-# else:
-#     a=line-X+1
-#     b=X
+if line%2==0:
+    a=X
+    b=line-X+1
+else:
+    a=line-X+1
+    b=X
     
-# print(a, '/', b, sep='')
+print(a, '/', b, sep='')
 
