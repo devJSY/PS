f6ab4bcbd678ecbffa4649b8c2d7f54307cf5db7
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2022-05-15 02:04:53 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	int H, M;
+
+	scanf("%d %d", &H, &M);
+	
+	M = M - 45;
+
+	if (M < 0)
+	{
+		H += -1;
+		M = 60 + M;
+	}
+
+	if (H < 0) H = 24 + H;
+
+	printf("%d %d", H, M);
+
+	return 0;
+}
