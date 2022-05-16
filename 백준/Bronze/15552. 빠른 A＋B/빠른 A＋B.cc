b4d69aa06d762b30095ed97cfc22e39c481f25fb
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2022-05-16 16:27:58 +0900

    [Bronze II] Title: 빠른 A+B, Time: 360 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	int i, T, A, B;
+
+	scanf("%d", &T);
+
+	for (i = 0; i < T; ++i)
+	{
+		scanf("%d %d", &A, &B);
+		printf("%d\n", A + B);
+	}
+
+	return 0;
+}
+
