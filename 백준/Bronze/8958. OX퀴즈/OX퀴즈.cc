e0058641244d508c1f991f294a71f58eb2c7b84c
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2022-05-30 00:32:02 +0900

    [Bronze II] Title: OX퀴즈, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+int main()
+{
+	int n, i, j, cnt, result;
+	string a;
+	
+	scanf("%d\n", &n);
+	
+	for(i=0; i < n; i++)
+	{	
+		cin >> a;
+		result = 0;
+		cnt = 0;
+		for (j = 0; a[j] != '\0'; ++j)
+		{
+			if (a[j] == 'O') cnt++;
+			else cnt = 0;
+			
+			result += cnt;
+		}
+		printf("%d\n", result);
+	}
+
+	return 0;
+}
