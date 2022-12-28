fbc62a2b9aaf10954db37549557e6e65664bb13b
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2022-12-28 20:17:55 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.cc
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -1,20 +1,23 @@
-#include<iostream>
+#include <iostream>
 
-int main() {
-	int i, n, a[1000] = {0, }, cnt = 0, k;
-
-	for (i = 0; i <= 9; ++i)
+int main()
+{
+	int a[100] = {}, n, i, cnt = 0;
+	
+	for(i =0; i < 10;++i)
 	{
 		scanf("%d", &n);
-		a[n % 42]++;
+		
+		++a[n % 42];
 	}
-
-	for (i = 0; i < 1000; ++i)
+	
+	for(i =0; i < 100;++i)
 	{
-		if (a[i] != 0) cnt++;
+		if(a[i] != 0) ++cnt;
 	}
-
+	
 	printf("%d", cnt);
-
+	
+	
 	return 0;
 }
