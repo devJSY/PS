17c283b6d4cd91cc33d24d487a6e75b54e193775
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-03-28 22:23:04 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 1156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,67 +1,126 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <string.h> 
 
-int s[10000]; //문제 조건에서 n<=10,000
-int tp=-1;
+int stack[10001] = {};
+int Index = 0;
 
-bool isFull() {
-    if(tp>=10000)
-        return true;
-    else
-        return false;
+void push(int X)
+{
+	stack[Index++] = X;
 }
 
-int isEmpty() {
-    if(tp==-1)
-        return 1;
-    else
-        return 0;
+void pop()
+{
+	--Index;
+
+	if (Index < 0)
+	{
+		Index = 0;
+		printf("%d\n", -1);
+	}
+	else
+	{
+		printf("%d\n", stack[Index]);
+		stack[Index] = -1;
+	}
 }
 
-void push() {
-    int i;
-    cin >> i;
-    if(!isFull())
-        s[++tp] = i;
-    //cout << s[tp] << endl;
+void size()
+{
+	int i = 0;
+
+	while (1)
+	{
+		if (stack[i] != -1)
+		{
+			++i;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	printf("%d\n", i);
 }
 
-int pop() {
-    if(!isEmpty()) {
-        return s[tp--];
-    }
-    else
-        return -1;
+void empty()
+{
+	int count = Index;
+	bool Trig = false;
+
+	while (count >= 0)
+	{
+		if (stack[count] != -1)
+		{
+			Trig = true;
+			break;
+		}
+		--count;
+	}
+
+	if (Trig)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		printf("%d\n", 1);
+	}
 }
 
-int size() {
-    return tp+1;
+void top()
+{
+	if (Index == 0)
+	{
+		printf("%d\n", -1);
+	}
+	else
+	{
+		printf("%d\n", stack[Index - 1]);
+	}
 }
 
-int top() {
-    if(tp != -1)
-        return s[tp];
-    else
-        return -1;
-}
+int main()
+{
+	int init = 10001 - 1;
 
+	while (init >= 0)
+	{
+		stack[init] = -1;
+		--init;
+	}
 
+	int input, n;
+	int i;
+	int num;
+	char wtd[100];
+	scanf("%d", &n);
 
-int main() {
-    int n;
-    string cmd;
-    cin >> n;
-    for(int i=0; i<n; i++) {
-        cin >> cmd;
-        if(cmd == "push")
-            push();
-        else if(cmd =="pop")
-            cout << pop() << endl;
-        else if(cmd == "size")
-            cout << size() << endl;
-        else if(cmd == "empty")
-            cout << isEmpty() << endl;
-        else if(cmd == "top")
-            cout << top() << endl;
-    }
+	for (i = 0; i < n; i++) {
+		num = 0;
+		scanf("%s", wtd);
+		if (strcmp(wtd, "push") == 0) {
+			scanf("%d", &input);
+			push(input);
+		}
+		else if (strcmp(wtd, "pop") == 0)
+		{
+			pop();
+		}
+		else if (strcmp(wtd, "size") == 0)
+		{
+			size();
+		}
+		else if (strcmp(wtd, "top") == 0)
+		{
+			top();
+		}
+		else if (strcmp(wtd, "empty") == 0)
+		{
+			empty();
+		}
+	}
+
+	return 0;
 }
