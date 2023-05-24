a3637684112317ab0d2bcf0acef7f3da32fd9952
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-05-24 15:24:32 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	for (int i = 0; i < n / 4; ++i)
+	{
+		cout << "long" << " ";
+	}
+
+	cout << "int" << endl;
+
+	return 0;
+}
+
+
