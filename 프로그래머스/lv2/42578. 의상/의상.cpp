2a2540a74a3d637f137e6bd26c0e92ba1fb6c50f
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-06-01 15:26:44 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) 
+{
+	unordered_map<string, int> map;
+
+	for (vector<string> vec : clothes)
+	{
+		map[vec[1]]++;
+	}
+
+	int answer = 1;
+
+	for (auto it = map.begin(); it != map.end(); it++)
+	{
+		answer *= it->second + 1;
+	}
+
+	return answer - 1;	
+}
