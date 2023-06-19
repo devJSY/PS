c2775284a95ed9604d78c55365e67b27c8a1bc17
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-06-19 12:38:18 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 48 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(pair<int, int>& a, pair<int, int>& b)
+{
+	if (a.first == b.first)
+	{
+		return a.second < b.second;
+	}
+	else return a.first < b.first;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0;
+	cin >> n;
+
+	vector<pair<int, int >> vec(n);
+
+	for (size_t i = 0; i < n; i++)
+	{
+		int x = 0;
+		int y = 0;
+		cin >> x >> y;
+		vec[i].first = x;
+		vec[i].second = y;
+	}
+
+	sort(vec.begin(), vec.end(), compare);
+
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << vec[i].first << " " << vec[i].second << "\n";
+	}
+
+	return 0;
+}
