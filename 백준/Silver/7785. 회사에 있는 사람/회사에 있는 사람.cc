e0e94729709bf2d14439e15bf56083b3ba988353
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-06-20 13:27:00 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 112 ms, Memory: 12984 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0;
+	cin >> n;
+
+	map<string, string, greater<string>> Map;
+
+	string name;
+	string access;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		cin >> name >> access;
+		if (Map.find(name) != Map.end())
+		{
+			Map.erase(name);
+		}
+		else
+		{
+			Map[name] = access;
+		}
+	}
+
+	for (auto iter : Map) {
+		cout << iter.first << '\n';
+	}
+
+	return 0;
+}
