1ba585ef7ab5f866993fcb64da05402490238c19
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-06-24 15:56:55 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.cc
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -4,13 +4,8 @@ using namespace std;
 
 int gcd(int a, int b)
 {
-    while (b > 0)
-    {
-        int tmp = a;
-        a = b;
-        b = tmp % b;
-    }
-    return a;
+	if (b == 0) return a;
+	return gcd(b, a % b);
 }
 
 int main()
