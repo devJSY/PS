3970d3bcc0406e519087c0b54de07246c2b116b5
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-07-07 18:46:11 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 112 ms, Memory: 12356 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+int visited[1000001] = {};
+int idx = 1;
+
+bool cmp(int a, int b)
+{
+	return a > b;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0;
+	int m = 0;
+	int r = 0;
+
+	cin >> n >> m >> r;
+
+	vector<vector<int>> vec(n + 1);
+
+	queue<int> Q;
+
+	int u = 0;
+	int v = 0;
+
+	for (size_t i = 0; i < m; i++)
+	{
+		cin >> u >> v;
+		vec[u].push_back(v);
+		vec[v].push_back(u);
+	}
+
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		sort(vec[i].begin(), vec[i].end(), greater<>());
+	}
+	
+	Q.push(r);	
+	visited[r] = idx++;
+
+	while (!Q.empty())
+	{
+		int num = Q.front();
+		Q.pop();
+
+		for (size_t i = 0; i < vec[num].size(); i++)
+		{
+			if (visited[vec[num][i]] == 0)
+			{
+				Q.push(vec[num][i]);
+				visited[vec[num][i]] = idx++;
+			}
+		}
+	}
+
+	for (size_t i = 1; i <= n; i++)
+	{
+		cout << visited[i] << '\n';
+	}
+
+
+	return 0;
+}
