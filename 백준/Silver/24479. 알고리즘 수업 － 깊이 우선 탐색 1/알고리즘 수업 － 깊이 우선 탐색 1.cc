889c8bf24b62e71b0f0f6c3625c54cab90c6aec7
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-07-07 18:23:22 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 92 ms, Memory: 12492 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int visited[100001] = {};
+int idx = 1;
+
+void DFS(vector<vector<int>>& vec, int r)
+{
+	visited[r] = idx;
+	++idx;
+	for (size_t i = 0; i < vec[r].size(); i++)
+	{
+		if (visited[vec[r][i]] == 0)
+		{
+			DFS(vec, vec[r][i]);
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0;
+	int m = 0;
+	int r = 0;
+	cin >> n >> m >> r;
+
+	vector<vector<int>> vec(n + 1);
+
+	int u = 0;
+	int v = 0;
+
+	for (size_t i = 0; i < m; i++)
+	{
+		cin >> u >> v;
+		vec[u].push_back(v);
+		vec[v].push_back(u);
+	}
+
+	for (size_t i = 1; i <= n; i++)
+	{
+		sort(vec[i].begin(), vec[i].end());
+	}
+
+	DFS(vec, r);
+
+	for (size_t i = 1; i <= n; i++)
+	{
+		cout << visited[i] << '\n';
+	}	
+
+	return 0;
+}
