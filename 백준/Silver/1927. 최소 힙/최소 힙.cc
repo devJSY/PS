6c0606ffbbffceef69e46d5f5c14bad37c47c1da
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-09-09 15:53:10 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    int n = 0;
+    cin >> n;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        int num = 0;
+        cin >> num;
+
+        if (num == 0)
+        {
+            if (pq.empty())
+                cout << 0 << '\n';
+            else
+            {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+        else
+        {
+            pq.push(num);
+        }
+    }
+  
+    return 0;
+}
