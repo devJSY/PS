55e1a509ecf3443fc7deaddc65fb0bcb9bfb9be9
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-10-14 13:53:01 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, count = 0;
+	cin >> n;
+	for (int i = 2; i <= n; i++) {
+		if (i % 5 == 0) count++;
+		if (i % 25 == 0) count++;
+		if (i % 125 == 0) count++;
+	}
+	cout << count;
+	
+	return 0;
+}
