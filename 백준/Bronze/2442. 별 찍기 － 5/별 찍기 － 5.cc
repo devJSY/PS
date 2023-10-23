0cc152c7a5f9c335048f5b948445b908c97211b4
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-10-23 21:35:55 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (size_t i = 1; i <= n; i++)
+	{
+		for (size_t j = 0; j < n - i; j++)
+		{
+			cout << " ";
+		}
+
+
+		for (size_t j = 0; j < (2 * i) - 1; j++)
+		{
+			cout << "*";
+		}
+		
+		cout << '\n';
+	}
+
+
+	return 0;
+}
+
