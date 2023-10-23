7fc96cfd4e6a6884ec7c1583c32a8650bc23042d
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-10-23 21:43:44 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -1,44 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    int n = 0;
-    cin >> n;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-   
+	int n;
+	cin >> n;
 
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            cout << " ";
-        }
-       
-        for (int k = 0; k < (i * 2) + 1; k++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+	for (size_t i = 1; i <= n; i++)
+	{
+		for (size_t j = 0; j < n - i; j++)
+		{
+			cout << " ";
+		}
 
-    for (int i = n - 1; i >= 0; i--)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            cout << " ";
-        }
+		for (size_t j = 0; j < (2 * i) - 1; j++)
+		{
+			cout << "*";
+		}
 
-        for (int k = 0; k < (i * 2) + 1; k++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
-   
+		cout << '\n';
+	}
 
-    return 0;
+	for (size_t i = 1; i <= n - 1; i++)
+	{
+		for (size_t j = 0; j < i; j++)
+		{
+			cout << " ";
+		}
+
+		for (size_t j = 0; j < (2 * (n - i)) - 1; j++)
+		{
+			cout << "*";
+		}
+
+		cout << '\n';
+	}
+
+
+	return 0;
 }
+
