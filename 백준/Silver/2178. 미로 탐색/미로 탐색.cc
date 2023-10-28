945a75276af28823e722e2b2190b36cbbc2fa79f
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-10-28 13:43:54 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2244 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,68 +1,61 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int N, M;                     
-int arr[101][101];
-int ch[101][101];
-int dist[101][101];
+int ch[105][105];
+int arr[105][105];
 
-int x_dir[4] = { -1, 1, 0, 0 };   
-int y_dir[4] = { 0, 0, -1, 1 };  
+int dx[4] = { 0, 1, 0 ,-1 };
+int dy[4] = { 1, 0, -1, 0 };
 
-queue<pair<int, int> > q;      
-
-// 미로 경로 탐색
-void bfs(int start_x, int start_y) {
-
-    ch[start_x][start_y] = 1;        
-    q.push(make_pair(start_x, start_y));    
-    dist[start_x][start_y]++;             
-
-    while (!q.empty()) {
-
-        int x = q.front().first;
-        int y = q.front().second;
-
-        q.pop();
-
-        for (int i = 0; i < 4; ++i) 
-        {
-            int x_new = x + x_dir[i];
-            int y_new = y + y_dir[i];
-
-            if ((0 <= x_new && x_new < N) && (0 <= y_new && y_new < M)
-                && !ch[x_new][y_new] && arr[x_new][y_new] == 1) {
-
-                ch[x_new][y_new] = 1;              
-                q.push(make_pair(x_new, y_new));        
-                dist[x_new][y_new] = dist[x][y] + 1;   
-            }
-        }
-    }
-}
-
-int main() 
+int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    cin >> N >> M;                     
+	queue<pair<pair<int, int>, int>> Q;
 
-    for (int i = 0; i < N; ++i) 
-    {            
-        string row;                   
-        cin >> row;
+	int n, m;
+	cin >> n >> m;
 
-        for (int j = 0; j < M; ++j) {      
-            arr[i][j] = row[j] - '0';   
-        }
-    }
+	for (size_t y = 1; y <= n; y++)
+	{
+		string num;
+		cin >> num;
 
-    bfs(0, 0); 
+		int idx = 1;
+		for (char ch : num)
+		{
+			arr[y][idx++] = ch - '0';
+		}
+	}
 
-    cout << dist[N - 1][M - 1];            
+	ch[1][1] = 1;
+	Q.push({ {1,1}, 1 });
 
-    return 0;
+	while (!Q.empty())
+	{
+		auto t = Q.front();
+		Q.pop();
+
+		for (size_t dir = 0; dir < 4; dir++)
+		{
+			int ny = t.first.first + dy[dir];
+			int nx = t.first.second + dx[dir];
+
+			if (ny < 1 || ny > n || nx < 1 || nx > m) continue;
+			if (ch[ny][nx] || arr[ny][nx] == 0) continue;
+
+			if (ny == n && nx == m)
+			{
+				cout << t.second + 1;
+				return 0;
+			}
+
+			ch[ny][nx] = 1;
+			Q.push({ {ny, nx}, t.second + 1 });
+		}
+	}
+
+	return 0;
 }
