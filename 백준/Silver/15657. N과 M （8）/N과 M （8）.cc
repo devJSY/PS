60fecb87e72129fee7aad5cecd3c563a5cd57be1
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2023-11-28 11:26:19 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int result[10];
+int arr[10];
+
+void DFS(int L, int idx)
+{
+    if (L == M)
+    {
+        for (size_t i = 0; i < M; i++)
+        {
+            cout << result[i] << " ";
+        }
+        cout << '\n';
+
+        return;
+    }
+
+    for (size_t i = idx; i < N; i++)
+    {
+        result[L] = arr[i];
+        DFS(L + 1, i);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+
+    for (size_t i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+
+    DFS(0, 0);
+
+    return 0;
+}
