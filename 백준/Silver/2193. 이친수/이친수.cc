d2488e3eb3492fd1b79a4a71de0a7615e26a8466
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2024-01-06 14:00:49 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -3,8 +3,6 @@
 using namespace std;
 
 long long dp[100] = {};
-long long zeroNum[100] = {};
-long long OneNum[100] = {};
 
 int main()
 {
@@ -12,18 +10,12 @@ int main()
     cin.tie(0);
 
     dp[1] = 1;
-    zeroNum[1] = 0;
-    OneNum[1] = 1;
 
     dp[2] = 1;
-    zeroNum[2] = 1;
-    OneNum[2] = 0;
 
     for (size_t i = 3; i <= 90; i++)
     {
-        dp[i] = (zeroNum[i - 1] * 2) + OneNum[i - 1];
-        zeroNum[i] = zeroNum[i - 1] + OneNum[i - 1];
-        OneNum[i] = zeroNum[i - 1];
+        dp[i] = dp[i - 2] + dp[i - 1];
     }
 
     int N;
