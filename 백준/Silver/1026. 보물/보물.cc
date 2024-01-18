6e689a2a2d0d9cf6f73969df8f3d42a6f7b3dc64
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2024-01-18 12:19:29 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    vector<int> Aarr(N);
+    vector<int> Barr(N);
+
+    for (size_t i = 0; i < N; i++)
+    {
+        cin >> Aarr[i];
+    }
+
+    for (size_t i = 0; i < N; i++)
+    {
+        cin >> Barr[i];
+    }
+
+    sort(Aarr.begin(), Aarr.end());
+    reverse(Aarr.begin(), Aarr.end());
+
+    long long result = 0;
+
+    for (size_t i = 0; i < N; i++)
+    {
+        vector<int>::iterator Aiter = min_element(Aarr.begin(), Aarr.end());
+        vector<int>::iterator Biter = max_element(Barr.begin(), Barr.end());
+        result += (*Biter) * (*Aiter);
+        Aarr.erase(Aiter);
+        Barr.erase(Biter);
+    }
+
+    cout << result;
+
+    return 0;
+}
