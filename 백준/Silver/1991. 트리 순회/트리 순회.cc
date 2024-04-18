1b8d38d370cf2164eb09daacd8733fa0876b286f
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2024-04-18 14:43:28 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,59 +1,64 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int a[50][2];
+int N;
+char lc[30];
+char rc[30];
 
-void preorder(int N) {
-    if (N == -1) return;
-    cout << (char)(N + 'A');
-    preorder(a[N][0]);
-    preorder(a[N][1]);
-}
-
-void inorder(int N) {
-    if (N == -1) return;
-    inorder(a[N][0]);
-    cout << (char)(N + 'A');
-    inorder(a[N][1]);
-}
-
-void postorder(int N) {
-    if (N == -1) return;
-    postorder(a[N][0]);
-    postorder(a[N][1]);
-    cout << (char)(N + 'A');
-}
-
-int main() 
+void preorder(int _Idx)
 {
-    ios_base::sync_with_stdio(0); 
+    if (0 == _Idx)
+        return;
+
+    cout << char(_Idx + 'A' - 1);
+    preorder(lc[_Idx]);
+    preorder(rc[_Idx]);
+}
+
+void inorder(int _Idx)
+{
+    if (0 == _Idx)
+        return;
+
+    inorder(lc[_Idx]);
+    cout << char(_Idx + 'A' - 1);
+    inorder(rc[_Idx]);
+}
+
+void postorder(int _Idx)
+{
+    if (0 == _Idx)
+        return;
+
+    postorder(lc[_Idx]);
+    postorder(rc[_Idx]);
+    cout << char(_Idx + 'A' - 1);
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
     cin.tie(0);
 
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		char x, y, z;
-		cin >> x >> y >> z;		// 알파벳 입력
-		x = x - 'A';		// 문자 -> 숫자 변환
-		if (y == '.') {		// 입력이 .이면 그 자리에 -1 대입
-			a[x][0] = -1;
-		}
-		else {
-			a[x][0] = y - 'A';
-		}
-		if (z == '.') {
-			a[x][1] = -1;
-		}
-		else {
-			a[x][1] = z - 'A';
-		}
-	}
-	preorder(0);
-	cout << '\n';
-	inorder(0);
-	cout << '\n';
-	postorder(0);
+    cin >> N;
 
-	return 0;
+    for (size_t i = 0; i < N; i++)
+    {
+        char p, l, r;
+
+        cin >> p >> l >> r;
+        if (l != '.')
+            lc[p - 'A' + 1] = l - 'A' + 1;
+        if (r != '.')
+            rc[p - 'A' + 1] = r - 'A' + 1;
+    }
+
+    preorder(1);
+    cout << '\n';
+    inorder(1);
+    cout << '\n';
+    postorder(1);
+
+    return 0;
 }
