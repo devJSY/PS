5339a6698f3d0e737e7dc4c33b8b5659ba65a7eb
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2024-05-22 20:03:38 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int result = 0;
+    while(n >= 1)
+    {
+        if(n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            --n;
+            ++result;
+        }        
+    }
+    
+    return result;
+}
