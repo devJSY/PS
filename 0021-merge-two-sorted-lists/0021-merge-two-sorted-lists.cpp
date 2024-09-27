33f47a67811e9e1b96e468061a9f94b9251b9cf8
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2024-09-27 15:30:37 +0900

    Time: 8 ms (30.41%), Space: 19.8 MB (16.92%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -3,47 +3,55 @@ class Solution
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
     {
-        deque<ListNode*> Q;
+        ListNode* result = nullptr;
+        ListNode* head = nullptr;
 
         while (list1 && list2)
         {
             if (list1->val <= list2->val)
             {
-                Q.push_back(list1);
+                if (result)
+                    result->next = list1;
+                else
+                    head = list1;
+
+                result = list1;
+
                 list1 = list1->next;
             }
             else
             {
-                Q.push_back(list2);
+                if (result)
+                    result->next = list2;
+                else
+                    head = list2;
+
+                result = list2;
+
                 list2 = list2->next;
             }
         }
 
         while (list1)
         {
-            Q.push_back(list1);
+            if (result)
+                result->next = list1;
+            else
+                head = list1;
+
+            result = list1;
             list1 = list1->next;
         }
 
         while (list2)
         {
-            Q.push_back(list2);
+            if (result)
+                result->next = list2;
+            else
+                head = list2;
+
+            result = list2;
             list2 = list2->next;
-        }
-
-        ListNode* head = nullptr;
-        if (!Q.empty())
-            head = Q.front();
-
-        while (!Q.empty())
-        {
-            ListNode* front = Q.front();
-            Q.pop_front();
-
-            if (!Q.empty())
-            {
-                front->next = Q.front();
-            }
         }
 
         return head;
