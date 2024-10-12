e4766a78bfaa3103b305ba6f9c6b4b5eba237db3
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2024-10-12 22:50:41 +0900

    Time: 2 ms (37.59%), Space: 7.9 MB (34.93%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int hammingWeight(int n)
+    {
+        int result = 0;
+
+        while (n > 0)
+        {
+            if (n & 1)
+            {
+                ++result;
+            }
+
+            n = n >> 1;
+        }
+
+        return result;
+    }
+};
