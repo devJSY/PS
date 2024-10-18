1e36318bd11d222d098d92b8ca4f92d76b5b2db6
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2024-10-18 17:05:50 +0900

    Time: 0 ms (100%), Space: 9 MB (40.79%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -7,25 +7,18 @@ public:
 
         for (int i = L; i < nums.size(); ++i)
         {
-            if (!visted[10 + nums[i]])
-            {
-                visted[10 + nums[i]] = true;
-                vec.push_back(nums[i]);
-                dfs(nums, i, vec);
-                vec.pop_back();
-                visted[10 + nums[i]] = false;
-            }
+            vec.push_back(nums[i]);
+            dfs(nums, i + 1, vec);
+            vec.pop_back();
         }
     }
 
     vector<vector<int>> subsets(vector<int>& nums)
     {
-        visted.resize(100);
         dfs(nums, 0, {});
         return result;
     }
 
 private:
     vector<vector<int>> result;
-    vector<bool> visted;
 };
