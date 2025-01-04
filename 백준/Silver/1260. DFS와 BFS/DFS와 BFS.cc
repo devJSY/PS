28ac7b1cca84c60d0e518de86b1f331ebc9289f9
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2025-01-04 15:13:02 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2304 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,76 +1,78 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> vec[1001];
-bool ch[1001];
-bool ch2[1001];
+int N, M, V;
+vector<vector<int>> Graph;
+vector<bool> visited;
 
-int n = 0;
-int m = 0;
-int v = 0;
-
-void DFS(int num)
+void dfs(int V)
 {
-    ch[num] = true;
-
-    cout << num << ' ';
-
-    for (size_t i = 0; i < vec[num].size(); i++)
+    cout << V << " ";
+    visited[V] = true;
+    for (int next : Graph[V])
     {
-        if(!ch[vec[num][i]])
-            DFS(vec[num][i]);
+        if (!visited[next])
+        {
+            dfs(next);
+        }
     }
 }
 
-int main() 
+void bfs(int V)
 {
-    ios_base::sync_with_stdio(0); 
-    cin.tie(0);
-
-
-    cin >> n >> m >> v;
-
-    for (int i = 0; i < m; i++)
-    {
-        int a = 0;
-        int b = 0;
-        cin >> a >> b;
-        vec[a].push_back(b);
-        vec[b].push_back(a);
-    }
-
-    for (int i = 1; i <= n; i++)
-    {
-        sort(vec[i].begin(), vec[i].end());
-    }
-
-    DFS(v);
-    cout << '\n';
-
     queue<int> Q;
-
-    Q.push(v);
-    ch2[v] = true;
+    Q.push(V);
+    visited[V] = true;
 
     while (!Q.empty())
     {
-        int num = Q.front();
-        cout << num << " ";
+        int nextV = Q.front();
         Q.pop();
+        cout << nextV << " ";
 
-        for (int i = 0; i < vec[num].size(); i++)
-        {            
-            if (!ch2[vec[num][i]])
+        for (int next : Graph[nextV])
+        {
+            if (!visited[next])
             {
-                Q.push(vec[num][i]);
-                ch2[vec[num][i]] = true;
+                Q.push(next);
+                visited[next] = true;
             }
         }
     }
-  
+}
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M >> V;
+
+    Graph.resize(N + 1);
+    visited.resize(N + 1, false);
+
+    for (int i = 0; i < M; ++i)
+    {
+        int u, v;
+        cin >> u >> v;
+        Graph[u].push_back(v);
+        Graph[v].push_back(u);
+    }
+
+    for (int i = 1; i <= N; ++i)
+    {
+        sort(Graph[i].begin(), Graph[i].end());
+    }
+
+    dfs(V);
+    visited.assign(N + 1, false);
+    cout << '\n';
+    bfs(V);
+
     return 0;
 }
