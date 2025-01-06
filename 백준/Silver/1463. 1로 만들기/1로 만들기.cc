6dff94c2e5ef7e8c7a838b10ac6799b6030c5c2b
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2025-01-06 21:22:03 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -14,19 +14,17 @@ int main()
 
     for (int i = 4; i <= N; ++i)
     {
-        DP[i] = 987654321;
-
-        if (i % 3 == 0)
-        {
-            DP[i] = min(DP[i], DP[i / 3] + 1);
-        }
+        DP[i] = DP[i - 1] + 1;
 
         if (i % 2 == 0)
         {
             DP[i] = min(DP[i], DP[i / 2] + 1);
         }
 
-        DP[i] = min(DP[i], DP[i - 1] + 1);
+        if (i % 3 == 0)
+        {
+            DP[i] = min(DP[i], DP[i / 3] + 1);
+        }
     }
 
     cout << DP[N];
