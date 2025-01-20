f68af4294715a55ac0596c6e5575ec388a29445b
Seungyeon Jung <90514882+devJSY@users.noreply.github.com>
2025-01-20 20:34:04 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int DP[1005];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    DP[1] = 1;
+    DP[2] = 3;
+
+    for (int i = 3; i <= 1000; ++i)
+    {
+        DP[i] = (DP[i - 1] + DP[i - 2] + DP[i - 2]) % 10007;
+    }
+
+    int n;
+    cin >> n;
+    cout << DP[n];
+
+    return 0;
+}
